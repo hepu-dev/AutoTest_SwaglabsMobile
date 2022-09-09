62a41690cdf93816a686849675c3dd93870feb2b
herrr14 <herupurnama47.hp@gmail.com>
2022-09-09 15:57:36 +0700

    push

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>93d1bf49-bb83-4e49-b80f-3a182f2c4eb2</testSuiteGuid>
+   <testCaseLink>
+      <guid>0a26e95a-3e81-46a3-ac10-0c339a3ed428</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC_02_Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>7ec545a6-a1ea-47f3-9ee8-bad1a90bc5af</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DF_Login</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>7ec545a6-a1ea-47f3-9ee8-bad1a90bc5af</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>842007c4-7669-4f09-8fb7-0200d116aff7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>7ec545a6-a1ea-47f3-9ee8-bad1a90bc5af</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>a1301867-bfe3-4ead-b4d1-232f5e8e7200</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>08f66028-5f86-4e85-8141-02ceacce6d50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Chart/TC_02_Chart</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
